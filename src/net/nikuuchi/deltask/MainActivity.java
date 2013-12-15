c75054aaeade1da3591b6c7a75cf81024ba92c2e
uchida <uchida.atsushi.8@gmail.com>
2013-12-15 13:46:24 +0900

    work action bar icon

--- src/net/nikuuchi/deltask/MainActivity.java
+++ src/net/nikuuchi/deltask/MainActivity.java
@@ -8,6 +8,7 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -53,6 +54,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_new_task:
+			Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
